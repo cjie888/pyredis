1cd0eb9c698b995dd8ff524911be4a5287f5e649
huchengjie <huchengjie@dianxinos.com>
2014-06-23 15:05:25 +0800

    encapsulated some command

--- shard_redis.py
+++ shard_redis.py
@@ -8,11 +8,9 @@ hasher = pyhash.murmur2_x64_64a()
     this is a shard redis which transplant from jedis(redis java client)
     Example:
         serverInfos = ["10.18.105.119:6379", "10.18.105.119:6479"]
-        sharedJedis = ShardJedis(serverInfos)
+        sharded_redis = ShardJedis(serverInfos)
         key="test"
-        redis_client = sharedJedis.getRedis(key)
-        redis_client.set(key,"value")
-        print key,redis_client.get(key)
+        sharded_redis.delete(key)
 '''
 class ShardJedis():
 
@@ -62,4 +60,62 @@ class ShardJedis():
         redisInfo = self.getShardInfo(key)
         redisInfos = redisInfo.split(":")
         #print redisInfo
-        return redis.StrictRedis(host = redisInfos[0],port = int(redisInfos[1]))
+        return redis.StrictRedis(host = redisInfos[0], port = int(redisInfos[1]))
+
+    def delete(self, key):
+        redis_client = self.getRedis(key)
+        return redis_client.delete(key)
+
+    def set(self, key, value):
+        redis_client = self.getRedis(key)
+        return redis_client.set(key,value)
+
+    def get(self, key):
+        redis_client = self.getRedis(key)
+        return redis_client.get(key)
+
+    def exists(self, key):
+        redis_client = self.getRedis(key)
+        return redis_client.exists(key)
+
+    def expire(self, key, seconds):
+        redis_client = self.getRedis(key)
+        return redis_client.expire(key, seconds)
+
+    def expireat(self, key, when):
+        redis_client = self.getRedis(key)
+        return redis_client.expireat(key, when)
+
+    def ttl(self, key):
+        redis_client = self.getRedis(key)
+        return redis_client.ttl(key)
+
+    def lrange(self, key, start, end):
+        redis_client = self.getRedis(key)
+        return redis_client.lrange(key, start, end)
+
+    def incr(self, key, amount=1):
+        redis_client = self.getRedis(key)
+        return redis_client.incr(key, amount)
+
+    def getset(self, key, newvalue):
+        redis_client = self.getRedis(key)
+        return redis_client.getset(key, newvalue)
+
+    def llen(self, key):
+        redis_client = self.getRedis(key)
+        return redis_client.llen(key)
+
+    def hincrby(self, key, hash_key, amount=1):
+        redis_client = self.getRedis(key)
+        return redis_client.hincrby(key,hash_key, amount)
+
+    def zrevrange(self, key, start, end):
+        redis_client = self.getRedis(key)
+        return redis_client.zrevrange(key, start, end)
+
+def main():
+        serverInfos = ["127.0.0.1:6379", "127.0.0.1:6479"]
+        sharded_redis = ShardJedis(serverInfos)
+        key="test"
+        sharded_redis.delete(key)
